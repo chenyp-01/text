65ea8fc67d9a5fcb8af7ab8ce3f08b74f1f4c16b
cyp <chen19840879870@163.com>
2025-05-08 10:43:00 +0800

    最新

---
+++ eslint.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential', // Vue 3 基本规则
+    'plugin:prettier/recommended', // 启用 Prettier 规则
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+});
